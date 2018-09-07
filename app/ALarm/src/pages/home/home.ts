f6f21610258a499a96cd56a1eca29372af66ead8
Marco Woldering <marcowoldering@gmail.com>
2018-09-07 20:53:36 +0200

    fix

--- app/ALarm/src/pages/home/home.ts
+++ app/ALarm/src/pages/home/home.ts
@@ -10,37 +10,31 @@ export class HomePage {
   events:any;
 
   constructor(public navCtrl: NavController) {
-    console.log("tetgd", this.getEvents());
-    this.events = this.getEvents();
+    this.events = [
+        {
+            "event": "metal_alarm",
+            "id": "de305d54-75b4-431b-adb2-eb6b9e546014",
+            "time": "2018-09-07T14:43:57.351Z",
+            "group": "A",
+            "units": [1],
+            "image": "https://picsum.photos/200?random"
+        },
+        {
+            "event": "metal_alarm",
+            "id": "2de5ff5a-29fd-4e46-8ee1-ff2538be72f5",
+            "time": "2018-09-07T15:43:57.351Z",
+            "group": "A",
+            "units": [1],
+            "image": "https://picsum.photos/200?random"
+        },
+        {
+            "event": "metal_alarm",
+            "id": "267e30e6-5963-4fad-81c9-9f3b4462c21a",
+            "time": "2018-09-07T15:53:57.351Z",
+            "group": "A",
+            "units": [1],
+            "image": "https://picsum.photos/200?random"
+        }
+    ]
   }
-
-  getEvents() {
-      return [
-          {
-              "event": "metal_alarm",
-              "id": "de305d54-75b4-431b-adb2-eb6b9e546014",
-              "time": "2018-09-07T14:43:57.351Z",
-              "group": "A",
-              "units": [1],
-              "image": "https://picsum.photos/200?random"
-          },
-          {
-              "event": "metal_alarm",
-              "id": "2de5ff5a-29fd-4e46-8ee1-ff2538be72f5",
-              "time": "2018-09-07T15:43:57.351Z",
-              "group": "A",
-              "units": [1],
-              "image": "https://picsum.photos/200?random"
-          },
-          {
-              "event": "metal_alarm",
-              "id": "267e30e6-5963-4fad-81c9-9f3b4462c21a",
-              "time": "2018-09-07T15:53:57.351Z",
-              "group": "A",
-              "units": [1],
-              "image": "https://picsum.photos/200?random"
-          }
-      ]
-  }
-
 }
